315b86088fefce8c20117edf58a0ce00619f97ea
ici906 <ddhhyy1@gmail.com>
2022-12-21 11:57:49 +0900

    임시구현

---
+++ StudyCafeProject2/src/test/java/com/studycafe/prac/StudyCafeProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studycafe.prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCafeProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
